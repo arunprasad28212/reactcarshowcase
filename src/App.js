4018b86063d78d61aca8b21dfa0e44c74dc87bf8
arunprasad28212 <arunprasad28212@gmail.com>
2023-11-30 12:56:31 +0530

    font

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header font-bold">
         <img src={logo} className="App-logo" alt="logo" />
         <p className='text-[40px] text-[#242424]'>
           Edit <code>src/App.js</code> and save to reload.
